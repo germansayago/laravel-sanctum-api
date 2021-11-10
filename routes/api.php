55742b5c7b519a72a3859006bd2f0d6d89d86e7a
germansayago <german.sayago@gmail.com>
2021-11-10 14:33:18 -0300

    add Product model and controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+// use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products/', function(){
-    return 'products';
-});
+
+Route::resource('products', ProductController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
